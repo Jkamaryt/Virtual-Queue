ba1569159500ff25956306630969cf4e7e8825de
Jack Kamaryt <jkamaryt46@gmail.com>
2023-03-03 09:38:32 -0600

    Initial Commit

---
+++ Computer Science Queue/Computer_Science_QueueApp.swift
@@ -0,0 +1,17 @@
+//
+//  Computer_Science_QueueApp.swift
+//  Computer Science Queue
+//
+//  Created by Jack Kamaryt on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Computer_Science_QueueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Computer Science Queue/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Computer Science Queue
+//
+//  Created by Jack Kamaryt on 3/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
