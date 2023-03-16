a9b4bf067cb50802821c05382fd2e256cf949d62
Jack Kamaryt <jkamaryt46@gmail.com>
2023-03-15 23:34:19 -0500

    Have not committed in a long while, but, here is the working code with everything I want in this app besides ordering the rows by color. When attempted to order them by color it cause lag in the app and didn't seem so seamless.

---
+++ Computer Science Queue/AddQueue.swift
@@ -0,0 +1,106 @@
+//
+//  AddQueue.swift
+//  Computer Science Queue
+//
+//  Created by Jack Kamaryt on 3/9/23.
+//
+
+import SwiftUI
+
+struct AddQueue: View {
+    @Environment(\.presentationMode) var presentationMode
+    
+    @State private var position: Int = 0
+    @State private var name: String = ""
+    @State private var notes: String = ""
+    @State private var colorPicker = ""
+    static let colorPicker = ["Green", "Yellow", "Red"]
+    
+    var body: some View {
+        VStack{
+            Text("Add Name")
+                .padding()
+            
+            Form{
+                VStack{
+                    HStack{
+                        Text("Name:")
+                        TextField("name",text: $name)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    Picker("Color:", selection: $colorPicker){
+                        ForEach(Self.colorPicker, id: \.self) { color in
+                            Text(color)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    HStack{
+                        Text("Notes:")
+                        TextField("notes",text: $notes)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    Button("Add") {
+                        Task {
+                            await postData()
+                            presentationMode.wrappedValue.dismiss()
+                            //DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                            //    Task {
+                            //        await getData()
+                             //   }
+                           // }
+                        }
+                    }
+                }
+            }
+            Spacer()
+            Button {
+                presentationMode.wrappedValue.dismiss()
+            } label: { Text("Back") }
+                .position(x:30, y:-390)
+        }
+    }
+    
+    
+    func postData() async {
+        let url = URL(string: "https://script.google.com/macros/s/AKfycbxelvNYSJ8q7DuVDmN0IkoatGrtS9KCXr4QeMZONoFfODSXTjAPGEdURCINcelzJgjHGw/exec")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        let data = ["value2": "\(name)", "value3": "\(colorPicker)", "value4": "\(notes)"]
+        let jsonData = try! JSONSerialization.data(withJSONObject: data, options: [])
+        request.httpBody = jsonData
+
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { data, response, error in
+          if let error = error {
+            print(error.localizedDescription)
+            return
+          }
+
+          guard let response = response as? HTTPURLResponse,
+                (200...299).contains(response.statusCode) else {
+            print("Server Error")
+            return
+          }
+
+          guard let data = data,
+                let jsonString = String(data: data, encoding: .utf8) else {
+            print("No Data")
+            return
+          }
+
+          print(jsonString)
+        }
+
+        task.resume()
+
+    }
+    
+}
+
+struct AddQueue_Previews: PreviewProvider {
+    static var previews: some View {
+        AddQueue()
+    }
+}
--- Computer Science Queue/ContentView.swift
+++ Computer Science Queue/ContentView.swift
@@ -1,50 +1,101 @@
-//
-//  ContentView.swift
-//  Computer Science Queue
-//
-//  Created by Jack Kamaryt on 3/3/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     @State private var queue = [Queue]()
     @State private var showingAlert = false
+    @State private var showingAddQueue = false
+    @State private var position: Int = 0
+    @State private var name: String = ""
+    @State private var notes: String = ""
+    @State private var colorPicker = ""
+    @State private var isTimerRunning = false // added state variable for timer
+        
+        let timer = Timer.publish(every: 5, on: .main, in: .common)
+            .autoconnect()
+        
+        // Start the timer
+        func startTimer() {
+            isTimerRunning = true
+        }
+        
+        // Pause the timer
+        func pauseTimer() {
+            isTimerRunning = false
+        }
+        
+        // Handler for timer tick
+        func onTimerTick() {
+            guard isTimerRunning else { return }
+            Task { await getData() }
+        }
     
-    @State private var showingAddView = false
-    @Environment(\.presentationMode) var presentationMode
+    static let colorPicker = ["Green", "Yellow", "Red"]
+    
     var body: some View {
-        NavigationView {
-            List(queue){ element in
-                NavigationLink{
-                    Text("Notes:")
-                        .font(.title).bold()
-                        .position(x:50, y:10)
-                    Text(element.notes)
-                        .position(x:200, y:-300)
-                    
-                } label: {
-                    Text("\(element.position).")
-                    Text(element.name)
-                        .font(.none).bold()
-                    Image(element.color)
-                        .resizable()
-                        .frame(width:15, height:15)
+            NavigationView {
+                List {
+                    ForEach(queue) { element in
+                        NavigationLink {
+                            Text("Notes:")
+                                .font(.title)
+                                .bold()
+                                .position(x: 50, y: 10)
+                            Text(element.notes)
+                                .position(x: 200, y: -300)
+                        } label: {
+                            Text("\(element.position).")
+                            Text(element.name)
+                                .font(.none)
+                                .bold()
+                            Image(element.color)
+                                .resizable()
+                                .frame(width: 15, height: 15)
+                        }
+                    }
+                    //.onDelete() -> get whats deleted and assign it to rowId as a string
+                    .onDelete { indexSet in
+                        let rowId = queue[indexSet.first!].position
+                        queue.remove(atOffsets: indexSet)
+                        deleteRow(rowId: String(rowId))
+                    }
+
                 }
+                
+                
+                .fullScreenCover(isPresented: $showingAddQueue, content: {
+                    AddQueue()
+                        .onDisappear {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                                Task {
+                                    await getData()
+                                    startTimer() // start timer when queue is updated
+                                }
+                            }
+                        }
+                })
+                .navigationBarTitle("Virtual Queue", displayMode: .inline)
+                .navigationBarItems(leading: EditButton().onTapGesture { pauseTimer() }) // pause timer when edit button is clicked
+                .navigationBarItems(trailing: Button(action: {
+                    showingAddQueue = true
+                    pauseTimer() // pause timer when plus button is clicked
+                }) {
+                    Image(systemName: "plus")
+                })
+                .onAppear {
+                    Task { await getData() }
+                    startTimer() // start timer when content view is displayed
+                }
+                .onReceive(timer, perform: { _ in onTimerTick() })
             }
-            .sheet(isPresented: $showingAddView, content: {
-                AddView()})
-            .navigationBarTitle("Virtual Queue", displayMode: .inline)
+            .alert(isPresented: $showingAlert) {
+                Alert(title: Text("Loading Error"),
+                      message: Text("There was a problem loading the Queue"),
+                      dismissButton: .default(Text("OK")))
+            }
         }
-        .task {await getData()}
-        .alert(isPresented: $showingAlert) {
-            Alert(title: Text("Loading Error"),
-                  message: Text("There was a problem loading the Queue"),
-                  dismissButton: .default(Text("OK")))
-        }
-    }
+    
     func getData() async {
-        let query = "https://script.google.com/macros/s/AKfycbwb77J6271EkJPKhfLk7z9r1lurnVNExbXRM6UaQ-GzX0uH1BPOcXsTZCuAvweMYu6OWQ/exec"
+        let query = "https://script.google.com/macros/s/AKfycbxelvNYSJ8q7DuVDmN0IkoatGrtS9KCXr4QeMZONoFfODSXTjAPGEdURCINcelzJgjHGw/exec"
         if let url = URL(string: query) {
             if let (data, _) = try? await URLSession.shared.data(from: url) {
                 if let decodedResponse = try? JSONDecoder().decode(Info.self, from: data) {
@@ -55,6 +106,31 @@ struct ContentView: View {
         }
         showingAlert = true
     }
+    
+    func deleteRow(rowId: String) {
+        let scriptURL = "https://script.google.com/macros/s/AKfycbxelvNYSJ8q7DuVDmN0IkoatGrtS9KCXr4QeMZONoFfODSXTjAPGEdURCINcelzJgjHGw/exec"
+        guard let url = URL(string: scriptURL) else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let json = ["position": rowId]
+        guard let jsonData = try? JSONSerialization.data(withJSONObject: json) else { return }
+        request.httpBody = jsonData
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print("Error:", error.localizedDescription)
+                return
+            }
+            guard let data = data else { return }
+            let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+            if let responseJSON = responseJSON as? [String: Any], let success = responseJSON["success"] as? Bool, success {
+                print("Row deleted successfully")
+            }
+        }
+        task.resume()
+    }
+
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -69,7 +145,7 @@ struct Queue: Identifiable, Codable {
     var name: String
     var color: String
     var notes: String
-   
+    
     enum CodingKeys: String, CodingKey {
         case position = "Position"
         case name = "Name"
